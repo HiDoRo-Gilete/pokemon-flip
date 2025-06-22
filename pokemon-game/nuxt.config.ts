33b5e3caa54f49b38bb361232fcb3ed3c137ced2
HiDoRo-Gilete <127959236+HiDoRo-Gilete@users.noreply.github.com>
2025-06-22 18:13:00 +0700

    fix

--- pokemon-game/nuxt.config.ts
+++ pokemon-game/nuxt.config.ts
@@ -6,7 +6,7 @@ import { resolve } from 'path'
 export default defineNuxtConfig({
   devServer: {
     host: '0.0.0.0',  // (tuỳ chọn) cho phép truy cập từ LAN
-    port: 4000        // port mà `nuxt dev` sẽ listen
+    port: Number(process.env.PORT) ||3000        // port mà `nuxt dev` sẽ listen
   },
   compatibilityDate: '2024-11-01',
   alias: {
