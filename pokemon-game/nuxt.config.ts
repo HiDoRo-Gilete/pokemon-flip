1ab8396a876ba87e55beddd6a8000f79180b20bb
HiDoRo-Gilete <127959236+HiDoRo-Gilete@users.noreply.github.com>
2025-06-22 15:19:27 +0700

    fix

--- pokemon-game/nuxt.config.ts
+++ pokemon-game/nuxt.config.ts
@@ -2,6 +2,10 @@
 import tailwindcss from "@tailwindcss/vite";
 import {resolve} from "path"
 export default defineNuxtConfig({
+  server: {
+    host: '0.0.0.0',                // allow external connections
+    port: Number(process.env.PORT)  // Render will set process.env.PORT
+  },
   compatibilityDate: "2024-11-01",
   alias:{
     "assets": resolve(__dirname,"/assets"),
